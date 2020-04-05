bc37e40a9ca9a8ec68e7d3c2660ce263aa40d67b
Berta Balázs <95berta.balazs@gmail.com>
2020-04-05 18:54:16 +0200

    Wawe spawner
    Simple wave spawner

---
+++ Assets/WaveSpawner.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveSpawner : MonoBehaviour
+{
+    public Transform enemyPrefab;
+    public Transform spawnLoc;
+
+    public float timeBetweenWaves = 3f;
+    private float countdown = 2f;
+
+    public Text CountdownText;
+
+    private int waveIndex = 0;
+
+    void Update()
+    {
+        if (countdown <= 0f)
+        {
+            StartCoroutine(SpawnWave());
+            countdown = timeBetweenWaves;
+        }
+
+        countdown -= Time.deltaTime;
+        CountdownText.text = Mathf.Round(countdown).ToString();
+    }
+
+    IEnumerator SpawnWave()
+    {
+        waveIndex++;
+
+        for (int i = 0; i < waveIndex; i++)
+        {
+            SpawnEnemy();
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        Instantiate(enemyPrefab, spawnLoc.position, spawnLoc.rotation);
+    }
+}
