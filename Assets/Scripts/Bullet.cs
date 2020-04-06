7931ff88a6678ecad7dc05b7f436bbba7685e0a2
Berta Balázs <95berta.balazs@gmail.com>
2020-04-06 16:01:57 +0200

    Explosion effect

--- Assets/Scripts/Bullet.cs
+++ Assets/Scripts/Bullet.cs
@@ -6,6 +6,7 @@ public class Bullet : MonoBehaviour
     private Transform target;
 
     public float speed = 70f;
+    public float explosionRadius = 0f;
     public GameObject impactEffect;
 
     public void Seek(Transform _target)
@@ -37,16 +38,49 @@ public class Bullet : MonoBehaviour
             return;
         }
 
+        // Moves the bullet to the target
         transform.Translate(dir.normalized * distanceThisFrame, Space.World);
-
+        // Rotate the bullet to the target direction.
+        transform.LookAt(target);
     }
 
     private void HitTarget()
     {
         GameObject effectIns = (GameObject)Instantiate(impactEffect, transform.position, transform.rotation);
-        Destroy(effectIns, 2f);
+        Destroy(effectIns, 5f);
 
-        Destroy(target.gameObject);
+        if (explosionRadius > 0f)
+        {
+            Explode();
+        }
+        else
+        {
+            Damage(target);
+        }
+
         Destroy(gameObject);
+    }
+
+    void Explode()
+    {
+        Collider[] colliders = Physics.OverlapSphere(transform.position, explosionRadius);
+        foreach (var collider in colliders)
+        {
+            if (collider.tag == "Enemy")
+            {
+                Damage(collider.transform);
+            }
+        }
+    }
+
+    void Damage(Transform enemy)
+    {
+        Destroy(enemy.gameObject);
+    }
+
+    void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
     }
 }
