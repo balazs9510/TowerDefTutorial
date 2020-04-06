6be66265d6fce4960aab72e662660be0482d90a2
Berta Balázs <95berta.balazs@gmail.com>
2020-04-06 11:54:37 +0200

    Buildable turrets

---
+++ Assets/Scripts/BuildManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        instance = this;
+    }
+
+    public GameObject standardTurretPrefab;
+
+    private GameObject turretToBuild;
+
+    public GameObject GetTurretToBuild()
+    {
+        return turretToBuild;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        turretToBuild = standardTurretPrefab;
+    }
+}
---
+++ Assets/Scripts/Node.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    public Color hoverColor;
+    public Vector3 offset;
+
+    private GameObject turret;
+
+    private Renderer rend;
+    private Color defaultColor;
+
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        defaultColor = rend.material.color;
+    }
+
+    void OnMouseDown()
+    {
+        if(turret != null)
+        {
+            Debug.Log("Can't build there");
+            return;
+        }
+
+        GameObject turretToBuild = BuildManager.instance.GetTurretToBuild();
+        turret = Instantiate(turretToBuild, transform.position + offset, transform.rotation);
+    }
+
+    void OnMouseEnter()
+    {
+        rend.material.color = hoverColor;
+    }
+
+    void OnMouseExit()
+    {
+        rend.material.color = defaultColor;
+    }
+
+}
